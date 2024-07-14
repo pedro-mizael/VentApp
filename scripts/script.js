10539764ffb084ca56179add2036b9fd34c0db11
Pedro Mizael <110135857+pedro-mizael@users.noreply.github.com>
2024-07-14 18:46:14 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function login() {
+    document.getElementById('login').style.width = "100%";
+    document.getElementById('login_tab').style.width = "100%";
+    document.getElementById('navbar').style.display = "none";
+}
+
+function voltar() {
+	window.location.replace(window.location);
+}
+
+function chats() {
+    document.getElementById('chats').style.width = "100%";
+    document.getElementById('chats_tab').style.width = "100%";
+}
+
+function homepage() {
+	window.location.replace(window.location);
+}
+
+function network() {
+    document.getElementById('network').style.width = "100%";
+    document.getElementById('network_tab').style.width = "100%";
+    
+}
